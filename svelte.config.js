d92985baef246ac9fbe2eff6a427965076c87763
Jesus Marron <jesusmarron7@gmail.com>
2024-11-06 01:26:59 -0600

    build project as static site

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,14 @@ const config = {
 	],
 	
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+		}),
+		prerender: {
+			default: true,
+		},
 		alias: {
 			$content: 'src/content'
 		}
