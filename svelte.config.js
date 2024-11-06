11b5646f2bbd7b4fefcee45f7fa04779e4cdcd99
Jesus Marron <jesusmarron7@gmail.com>
2024-11-06 01:39:16 -0600

    using adapter-cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,11 +12,7 @@ const config = {
 	],
 	
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-		}),
+		adapter: adapter(),
 		prerender: {
 			default: true,
 		},
