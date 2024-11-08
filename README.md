389ecd6ddd43f39305be15fd7bd356442714d615
Jesus Marron <jesusmarron7@gmail.com>
2024-11-08 13:50:23 -0600

    does adding posthog work?

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # jesusmarron.com
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/b1f6a822-bd67-42dc-aa92-b7eb6ccb532a/deploy-status)](https://app.netlify.com/sites/jesusmarron/deploys)
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,28 @@
 <script lang="ts">
+	import posthog from 'posthog-js'
+	import { browser } from '$app/environment';
 	import '../app.css';
+	
+	const posthogApiKey = import.meta.env.VITE_POSTHOG_API_KEY;
+	const posthogApiHost = import.meta.env.VITE_POSTHOG_API_HOST;
+
+	export const load = async () => {
+		
+		if (browser) {
+			posthog.init(
+			posthogApiKey,
+			{ 
+				api_host: posthogApiHost,
+				person_profiles: 'always',
+				capture_pageview: true,
+				capture_pageleave: true,
+				capture_performance: true,
+			}
+			)
+		}
+		return
+	};
+	
 	let { children } = $props();
 </script>
 
