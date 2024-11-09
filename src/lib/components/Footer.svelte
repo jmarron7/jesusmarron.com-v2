792229531f16fa6c0d2a7686cf3233b7faf9e2eb
Jesus Marron <jesusmarron7@gmail.com>
2024-11-09 17:29:36 -0600

    footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,7 +5,7 @@
 <footer class="flex flex-col items-center text-sm">
   <div>
     <p>
-      Built with <a href="https://svelte.dev/" class="font-semibold hover:text-orange-500 focus-visible:text-orange-500" target="_blank" rel="noreferrer noopener" aria-label="Sveltekit (opens a new tab)">Sveltekit</a>. Deployed on <a href="https://netlify.com/" class="font-semibold hover:text-blue-500 focus-visible:text-blue-500" target="_blank" rel="noreferrer noopener" aria-label="Netlify (opens a new tab)">Netlify</a>. © {currentYear} Jesus Marron 
+      Built with <a href="https://svelte.dev/" class="font-semibold hover:text-orange-500 focus-visible:text-orange-500" target="_blank" rel="noreferrer noopener" aria-label="Sveltekit (opens a new tab)">Sveltekit</a>. Hosted on <a href="https://netlify.com/" class="font-semibold hover:text-blue-500 focus-visible:text-blue-500" target="_blank" rel="noreferrer noopener" aria-label="Netlify (opens a new tab)">Netlify</a>. © {currentYear} Jesus Marron 
     </p>
   </div>
 </footer>
