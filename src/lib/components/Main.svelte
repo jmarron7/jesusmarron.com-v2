57062337be109c6b86e43723c70a9d065890b5a9
Jesus Marron <jesusmarron7@gmail.com>
2024-11-09 17:28:06 -0600

    footer

--- src/lib/components/Main.svelte
+++ src/lib/components/Main.svelte
@@ -1,10 +1,12 @@
 <script>
 	import About from "./About.svelte";
 	import Experience from "./Experience.svelte";
+	import Footer from "./Footer.svelte";
 	import Projects from "./Projects.svelte";
 </script>
 <main class="pt-24 lg:w-1/2 lg:py-24">
 <About></About>
 <Experience></Experience>
 <!-- <Projects></Projects> -->
+ <Footer></Footer>
 </main>
