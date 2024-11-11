fa648365124cc0cfe83cb0e27247bae2526d8d06
Jesus Marron <jesusmarron7@gmail.com>
2024-11-11 15:06:12 -0600

    editing about section

--- src/lib/components/sections/About.svelte
+++ src/lib/components/sections/About.svelte
@@ -15,6 +15,6 @@
     that enable users to manage and perform supply chain operations across North America. I enjoy working across the stack, from database design to building interactive frontend components. My focus is often drawn to optimizing performance, ensuring reliability, and refining the overall user experience of the product.
   </p>
   <p class="text-surface-200 mb-4">
-    Lately, I've been obsessed with Go and Sveltekit and have been using both for various projects behind the scenes. Other things that I've been curious about lately are storage, graphics programming, and distributed systems.
+    Lately, I've been obsessed with Go and Sveltekit and have been using both for various projects behind the scenes. Other areas I've been exploring are storage, graphics programming, and distributed systems.
   </p>
 </section>
