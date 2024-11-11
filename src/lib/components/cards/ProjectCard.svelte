95db450ab87b99984d56fc562e1761d9bfaf9bdb
Jesus Marron <jesusmarron7@gmail.com>
2024-11-10 18:07:01 -0600

    no-image fallback on invalid image paths for project card

--- src/lib/components/cards/ProjectCard.svelte
+++ src/lib/components/cards/ProjectCard.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { Project } from "$lib/types/types";
-	import Pill from "../Pill.svelte";
+  import Pill from "../Pill.svelte";
   
   type Props = { project: Project }
   let { project }: Props = $props();
@@ -9,7 +9,18 @@
 <div class="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
   <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-surface-100/5 lg:group-hover:drop-shadow-lg"></div>
   <div class="z-10 mb-2 mt-1 text-sm font-semibold uppercase tracking-wide text-surface-300 sm:col-span-2">
-    <img src={"images/" + project.imagePath} alt={"Preview image of " + project.title} loading="lazy" width="200" height="48" decoding="async" class="rounded border-2 border-green-800/90 transition group-hover:border-green-500 sm:order-1 sm:col-span-2" />
+    <img 
+    src={`images/${project.imagePath}`} 
+    alt={"Preview image of " + project.title} 
+    loading="lazy" 
+    width="200" 
+    height="48" 
+    decoding="async" 
+    class="rounded border-2 border-green-800/90 transition group-hover:border-green-500 sm:order-1 sm:col-span-2" 
+    onerror={(event: Event) => {
+      const target = event.currentTarget as HTMLImageElement;
+      target.src = 'images/no-image.png';
+    }}/>
   </div>
   <div class="z-10 sm:col-span-6">
     <h3 class="font-medium leading-snug">
