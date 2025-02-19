f16c80ed328cac79cacae899756742be8025bb05
Jesus Marron <jesusmarron7@gmail.com>
2025-02-18 23:59:15 -0600

    updating experience

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -6,14 +6,14 @@ export const experienceData: Experience[] = [
     companyUrl: "https://www.fedex.com",
     startDate: " OCT 2023",
     endDate: "PRESENT",
-    description: "Developed full-stack applications and maintained legacy systems to support operations for logistics services. Improved processes through performance optimizations, reducing data export time significantly, and enhancing application observability and monitoring systems. Led the upgrade of an Angular 11 project to Angular 15, addressing deprecated libraries to meet security compliance standards.",
-    tech: ["Java", "Spring", "Angular", "Typescript", "MySQL", "Oracle DB", "Jenkins", "Cypress", "ReadyAPI", "Splunk", "Grafana", "AppDynamics"]
+    description: "Developed full-stack applications and microservices in an event-driven architecture to support operations for global logistics services. Improved processes through performance optimizations, reducing data export time significantly, and enhancing application observability and monitoring systems. Led the upgrade of an Angular 11 project to Angular 15, addressing deprecated libraries to meet security compliance standards.",
+    tech: ["Java", "Spring", "Angular", "Typescript", "MySQL", "Oracle DB", "Jenkins", "Github Actions", "Playwright", "ReadyAPI", "Splunk", "Grafana", "AppDynamics"]
   },
   {
     role: "Software Engineer, Platform Infrastructure",
     company: "Blackbaud",
     companyUrl: "https://www.blackbaud.com",
-    startDate: " JUNE 2023",
+    startDate: " JUNE 2022",
     endDate: "FEB 2023",
     description: "Led efforts in automating server management, streamlining processes for infrastructure migration, and reducing operational costs. Developed tools and systems to enhance infrastructure efficiency and collaborated with teams to create reusable automation libraries, contributing to a more effective development environment.",
     tech: ["Python", "Flask", "React", "Typescript", "Powershell", "Bash", "VMware vSphere", "Azure", "Infrastructure as Code"]
